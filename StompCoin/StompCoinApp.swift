0d1e8a7b8d56ec7a585b8fd10ad025510314db56
Fatih Acci, Oscar Artes, Shirley Davidsen <darboukaive,de>
2025-05-22 00:18:10 +0200

    Initial Commit

---
+++ StompCoin/StompCoinApp.swift
@@ -0,0 +1,32 @@
+//
+//  StompCoinApp.swift
+//  StompCoin
+//
+//  Created by fatih on 22.05.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StompCoinApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
