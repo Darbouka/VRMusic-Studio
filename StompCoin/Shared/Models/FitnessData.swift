c1a90923c0917c5896d1893d064333e28cadac7b
Fatih Acci, Oscar Artes, Shirley Davidsen <darboukaive,de>
2025-06-07 15:20:29 +0200

    StompCoin-Dateien entfernt: Projekttrennung VR DAW und StompCoin

--- StompCoin/Shared/Models/FitnessData.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct FitnessData: Codable {
-    var steps: Int
-    var calories: Double
-    var distance: Double
-    var activeMinutes: Int
-    var lastUpdated: Date
-} 
--- StompCoin/StompCoin/Android/app/src/main/java/com/stompcoin/app/StompCoinApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.stompcoin.app
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class StompCoinApplication : Application() 
